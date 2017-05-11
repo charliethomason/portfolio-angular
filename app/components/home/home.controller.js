3863c8ac826bd2a21d5037ebce014818cfffe35a
Charlie Thomason <charlie.thomason@uptake.com>
2017-05-11 15:09:55 -0500

    built out homepage and featured row

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -1,7 +1,7 @@
 angular.module('charlie')
     .controller('HomeController', HomeController);
 
-function HomeController($rootScope) {
+function HomeController(AppServices, $rootScope) {
     var vm = this;
 
     vm.$onInit = function() {
@@ -16,5 +16,14 @@ function HomeController($rootScope) {
 
     function page() {
         vm.heading = 'Charlie Thomason';
+
+        AppServices.getContent('art')
+            .then(function(response) {
+                vm.latestArt = response.data.works[1];
+            });
+        AppServices.getContent('photos')
+            .then(function(response) {
+                vm.latestPhoto = response.data.works[1];
+            });
     }
 }
